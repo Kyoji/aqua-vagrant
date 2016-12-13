23eee81be05973a2655b9394cd91a1099c7701db
Daniel Owens <kyoji1@gmail.com>
2016-12-13 00:23:45 -0500

    Added php7.0-mysql, xdebug, custom php.ini

--- provision/apt-get_installs.sh
+++ provision/apt-get_installs.sh
@@ -4,3 +4,5 @@ sudo apt-get install -y apache2
 sudo apt-get install -y php7.0
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -q -y install mysql-server
+sudo apt-get install -y php7.0-mysql
+sudo apt-get install -y php7.0-xdebug
