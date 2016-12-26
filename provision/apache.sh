252d6b2aeca59bd178332a8be006adefee0b93b2
Daniel Owens <kyoji1@gmail.com>
2016-12-26 14:22:21 -0500

    update

--- provision/apache.sh
+++ provision/apache.sh
@@ -2,6 +2,7 @@
 
 sudo cp /srv/config/apache/local.aqua.dev.conf /etc/apache2/sites-available/
 sudo a2ensite local.aqua.dev.conf
+sudo a2enmod rewrite
 sudo service apache2 reload
 #if [ -d /var/www/html ]; then
 #    sudo rm -r /var/www/html
